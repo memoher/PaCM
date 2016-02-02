21ffe791bcc81e783a0b084319aaae9c98aaf4d4
Julian Hern�ndez <jhernandez@tcn.com.ar>
2016-02-02 17:30:31 -0300

    Habilitamos permisos para Android 17

--- www/js/common.js
+++ www/js/common.js
@@ -370,10 +370,15 @@
             var self = this;
 
             if ((navigator) && (navigator.connection)) {
+                alert(navigator.connection.toString());
+                alert(11);
                 var networkState = navigator.connection.type;
+                alert(networkState);
                 if ((Connection)) {
+                    alert(12);
                     return !(networkState === Connection.NONE);
                 } else {
+                    alert(13);
                     return !(networkState === navigator.connection.NONE);
                 }
             } else {
