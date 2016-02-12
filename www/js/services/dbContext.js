eb5b1a5938e0d90046c18faecbf7a137028f7643
Julian Hern�ndez <jhernandez@tcn.com.ar>
2016-02-12 08:40:04 -0300

    Mejoras para evitar apertura de teclado en inputs con popup

--- www/js/services/dbContext.js
+++ www/js/services/dbContext.js
@@ -13,7 +13,7 @@
             }            
         };
         
-        var dbVersion = '2016.02.10.15.10';
+        var dbVersion = '2016.02.12.08.30';
 
         var addressServer = 'http://eccmant.emhesolutions.com/'; //'http://localhost:8100/api/'; //'http://192.168.0.12:57080/'; //
 
