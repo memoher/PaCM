1d998fe151d9ed6aff634b4ff36c7bb0fc5162ea
Julian Hernández R <julian_her@hotmail.com>
2016-04-11 16:48:13 -0500

    Cambio de servidor

--- www/js/services/dbSynchronizer.js
+++ www/js/services/dbSynchronizer.js
@@ -7,7 +7,7 @@
 
 		var debugMode = 1;
 
-        var addressServer = 'http://192.168.1.36:60080/'; //'http://localhost:8100/api/';
+        var addressServer = 'http://192.168.1.33:60080/'; //'http://localhost:8100/api/';
 
         var tablesForImport = [
             'AppSettings', 'AppFiles', 'AppKeys', 
